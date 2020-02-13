2589e93589ff4d9296d873a8c9ab191e7bd71258
Lanrey12 <alexolajide197@gmail.com>
2020-02-13 05:24:26 -0800

    backend

--- api/models/Hobbydb.js
+++ api/models/Hobbydb.js
@@ -26,6 +26,5 @@ module.exports = {
     },
 
   },
-  connection: 'heroku_postgres'
 };
 
--- config/models.js
+++ config/models.js
@@ -17,7 +17,7 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  //  connection: 'heroku_postgres',
+  connection: 'heroku_postgres',
 
   /***************************************************************************
   *                                                                          *
